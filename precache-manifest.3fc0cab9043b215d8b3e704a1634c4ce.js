7e82cb1149cf0836e212e3db7a695a0eb6bc4060
kapil jhajhria <jhajhria44@gmail.com>
2020-04-15 09:48:08 +0530

    Updates

---
+++ precache-manifest.3fc0cab9043b215d8b3e704a1634c4ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415451ed4cc8ca4e154e7bc2bd5541c2",
+    "url": "/movies-grid/index.html"
+  },
+  {
+    "revision": "f9d928e8d2fe060ebece",
+    "url": "/movies-grid/static/css/main.09de718f.chunk.css"
+  },
+  {
+    "revision": "2a02ce2ae7a5f6b0deb4",
+    "url": "/movies-grid/static/js/2.beae8ea6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-grid/static/js/2.beae8ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d928e8d2fe060ebece",
+    "url": "/movies-grid/static/js/main.16a1a3a1.chunk.js"
+  },
+  {
+    "revision": "6696c7db5f2c2bc40bc2",
+    "url": "/movies-grid/static/js/runtime-main.13627b78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movies-grid/static/media/logo.5d5d9eef.svg"
+  }
+]);
